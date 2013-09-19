63ccec2a1a30bff2c12f16ac1d3a18a4f6efb363
Rod Vagg <rod@vagg.org>
2013-09-19 14:32:16 +1000

    reformat, expose 'fail'

--- bustermove.js
+++ bustermove.js
@@ -4,72 +4,73 @@ var tap     = require('tap')
   , sinon   = require('sinon')
   , referee = require('referee')
 
-  , createAsyncTestFn = function (ctx, testFn) {
-      if (!testFn) return function (done) { done() }
-      if (testFn.length) return testFn.bind(ctx)
-      return function (done) {
-        testFn.call(ctx)
-        done()
-      }
+function createAsyncTestFn (ctx, testFn) {
+  if (!testFn) return function (done) { done() }
+  if (testFn.length) return testFn.bind(ctx)
+  return function (done) {
+    testFn.call(ctx)
+    done()
+  }
+}
+
+function createSetUp (ctx, preSetUp, setUp) {
+  preSetUp = createAsyncTestFn(ctx, preSetUp)
+  setUp    = createAsyncTestFn(ctx, setUp)
+  return function (done) {
+    preSetUp(function () {
+      setUp(done)
+    })
+  }
+}
+
+function createTearDown (ctx, postTearDown, tearDown) {
+  postTearDown = createAsyncTestFn(ctx, postTearDown)
+  tearDown     = createAsyncTestFn(ctx, tearDown)
+  return function (done) {
+    tearDown(function () {
+      postTearDown(done)
+    })
+  }
+}
+
+var buster = {
+  testCase: function (suiteName, tests, ctx, preSetUp, postTearDown) {
+    if (!ctx) {
+      ctx = sinon.sandbox.create({
+          injectInto    : null
+        , properties    : [ 'spy', 'stub', 'mock' ]
+        , useFakeTimers : false
+        , useFakeServer : false
+      })
     }
+    var setUp    = createSetUp(ctx, preSetUp, tests.setUp)
+      , tearDown = createTearDown(ctx, postTearDown, tests.tearDown)
 
-  , createSetUp = function (ctx, preSetUp, setUp) {
-      preSetUp = createAsyncTestFn(ctx, preSetUp)
-      setUp    = createAsyncTestFn(ctx, setUp)
-      return function (done) {
-        preSetUp(function () {
-          setUp(done)
-        })
-      }
-    }
-
-  , createTearDown = function (ctx, postTearDown, tearDown) {
-      postTearDown = createAsyncTestFn(ctx, postTearDown)
-      tearDown     = createAsyncTestFn(ctx, tearDown)
-      return function (done) {
-        tearDown(function () {
-          postTearDown(done)
-        })
-      }
-    }
-
-  , buster = {
-      testCase: function (suiteName, tests, ctx, preSetUp, postTearDown) {
-        if (!ctx) {
-          ctx = sinon.sandbox.create({
-              injectInto    : null
-            , properties    : [ 'spy', 'stub', 'mock' ]
-            , useFakeTimers : false
-            , useFakeServer : false
-          })
-        }
-        var setUp    = createSetUp(ctx, preSetUp, tests.setUp)
-          , tearDown = createTearDown(ctx, postTearDown, tests.tearDown)
-
-        Object.keys(tests).forEach(function (name) {
-          if (name != 'setUp' && name != 'tearDown') {
-            if (typeof tests[name] == 'object')
-              return buster.testCase(suiteName + ': ' + name, tests[name], ctx, setUp, tearDown)
-            var test = createAsyncTestFn(ctx, tests[name])
-              , cfg  = {}
-            if (ctx.timeout) cfg.timeout = ctx.timeout
-            tap.test(suiteName + ': ' + name, cfg, function (t) {
-              t.ok(true, suiteName + ': ' + name) // blank assert to tell tap we're really a test!
-              setUp(function () {
-                test(function () {
-                  tearDown(function () {
-                    ctx.restore()
-                    t.end()
-                  })
-                })
+    Object.keys(tests).forEach(function (name) {
+      if (name != 'setUp' && name != 'tearDown') {
+        if (typeof tests[name] == 'object')
+          return buster.testCase(suiteName + ': ' + name, tests[name], ctx, setUp, tearDown)
+        var test = createAsyncTestFn(ctx, tests[name])
+          , cfg  = {}
+        if (ctx.timeout) cfg.timeout = ctx.timeout
+        tap.test(suiteName + ': ' + name, cfg, function (t) {
+          t.ok(true, suiteName + ': ' + name) // blank assert to tell tap we're really a test!
+          setUp(function () {
+            test(function () {
+              tearDown(function () {
+                ctx.restore()
+                t.end()
               })
             })
-          }
+          })
         })
       }
-    }
+    })
+  }
+}
 
 module.exports         = buster
 module.exports.assert  = referee.assert
 module.exports.refute  = referee.refute
+module.exports.fail    = referee.fail
 module.exports.referee = referee
